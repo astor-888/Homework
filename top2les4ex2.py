f8006f95494bc178b8f88429e83bdb8f642ed586
astor-888 <astor.del.buki@gmail.com>
2026-04-24 22:11:21 +0300

    Update top2les4ex2.py

--- top2les4ex2.py
+++ top2les4ex2.py
@@ -1,1 +1,8 @@
-
+number = int(input("Введите любое пятизначное число: "))
+units = number % 10
+tens = (number // 10) % 10
+hundreds = (number // 100) % 10
+thousands = (number // 1000) % 10
+ten_thousands = number // 10000
+result = (tens ** units * hundreds) / (ten_thousands - thousands)
+print("result:", result)
