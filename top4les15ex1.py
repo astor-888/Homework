9c583de83d765efe8d0c8d6f6f0b12c528a7abb4
astor-888 <astor.del.buki@gmail.com>
2026-09-07 19:14:32 +0300

    Add files via upload

---
+++ top4les15ex1.py
@@ -0,0 +1,12 @@
+class Transport:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Autobus(Transport):
+    def __str__(self):
+        return f"Название автомобиля: {self.name} Скорость: {self.max_speed} Пробег: {self.mileage}"
+
+bus = Autobus("Renaul Logan", 180, 12)
+print(bus)
